d5c47f9ca6a429c428de47bf66fdaf794ead075e
Mithul <mithulmathi@gmail.com>
2016-10-19 01:43:44 +0530

    back to old collisions, removed live sys time and negated effect of force during collisions

--- physics.py
+++ physics.py
@@ -20,7 +20,7 @@ class Physics(object):
 	def get_time(self):
 		td = time.time()-self.last_moved
 		self.last_moved = time.time()
-		return float(td*self.scale)
+		return 0.1#float(td*self.scale)
 
 	def move(self):
 		t = self.get_time()
@@ -85,54 +85,62 @@ class Physics(object):
 				nvely = self.vely*math.sin(mangle)# + self.velx*math.cos(angle)
 			# if angle_deg == 0 : pdb.set_trace()
 		else:
-			# nvelx = self.velx*(self.mass-obj.mass) + 2*obj.mass*obj.velx
-			# nvely = self.vely*(self.mass-obj.mass) + 2*obj.mass*obj.vely
+			t = self.get_time()
+			self.velx = self.velx - self.fx*t
+			self.vely = self.vely - self.fy*t
 
-			# nvelx1 = obj.velx*(obj.mass-self.mass) + 2*self.mass*self.velx
-			# nvely1 = obj.vely*(obj.mass-self.mass) + 2*self.mass*self.vely
+			t = obj.get_time()
+			obj.velx = obj.velx - obj.fx*t
+			obj.vely = obj.vely - obj.fy*t
+
+			nvelx = self.velx*(self.mass-obj.mass) + 2*obj.mass*obj.velx
+			nvely = self.vely*(self.mass-obj.mass) + 2*obj.mass*obj.vely
+
+			nvelx1 = obj.velx*(obj.mass-self.mass) + 2*self.mass*self.velx
+			nvely1 = obj.vely*(obj.mass-self.mass) + 2*self.mass*self.vely
 
 			# if not (self.velx*obj.velx < 0 or self.vely*obj.vely < 0 or (self.x/obj.x < obj.velx/self.velx or self.y/obj.y < obj.vely/self.vely )):
 			# 	return
 
-			# ovel = (self.velx**2 + self.vely**2)**0.5
-			# ovel2 = (obj.velx**2 + obj.vely**2)**0.5
+			ovel = (self.velx**2 + self.vely**2)**0.5
+			ovel2 = (obj.velx**2 + obj.vely**2)**0.5
 
-			# angle2 = math.atan2(obj.vely, obj.velx) - mangle
+			angle2 = math.atan2(obj.vely, obj.velx) - mangle
 			
-			# nvel = (ovel*math.cos(angle)*(self.mass-obj.mass) + 2*obj.mass*ovel2*math.cos(angle2))/(self.mass+obj.mass)
-			# nvelx = nvel*math.cos(mangle) - ovel*math.sin(angle)*math.cos(mangle+math.radians(90))/(self.mass+obj.mass)
-			# nvely = nvel*math.sin(mangle) + ovel*math.sin(angle)*math.sin(mangle+math.radians(90))/(self.mass+obj.mass)
-			# nvel2 = (ovel2*math.cos(angle)*(obj.mass-self.mass) + 2*self.mass*ovel*math.cos(angle))/(obj.mass+self.mass)
-			# nvelx2 = nvel2*math.cos(mangle) + ovel2*math.sin(angle2)*math.cos(mangle+math.radians(90))/(self.mass+obj.mass)
-			# nvely2 = nvel2*math.sin(mangle) + ovel2*math.sin(angle2)*math.sin(mangle+math.radians(90))/(self.mass+obj.mass)
-			# # pdb.set_trace()
+			nvel = (ovel*math.cos(angle)*(self.mass-obj.mass) + 2*obj.mass*ovel2*math.cos(angle2))/(self.mass+obj.mass)
+			nvelx = nvel*math.cos(mangle) - ovel*math.sin(angle)*math.cos(mangle+math.radians(90))/(self.mass+obj.mass)
+			nvely = nvel*math.sin(mangle) + ovel*math.sin(angle)*math.sin(mangle+math.radians(90))/(self.mass+obj.mass)
+			nvel2 = (ovel2*math.cos(angle)*(obj.mass-self.mass) + 2*self.mass*ovel*math.cos(angle))/(obj.mass+self.mass)
+			nvelx2 = nvel2*math.cos(mangle) + ovel2*math.sin(angle2)*math.cos(mangle+math.radians(90))/(self.mass+obj.mass)
+			nvely2 = nvel2*math.sin(mangle) + ovel2*math.sin(angle2)*math.sin(mangle+math.radians(90))/(self.mass+obj.mass)
+			# pdb.set_trace()
 
-			# obj.velx = nvelx2
-			# obj.vely = nvely2
+			obj.velx = nvelx2
+			obj.vely = nvely2
 
 
-			xDist = self.x - obj.x
-			yDist = self.y - obj.y
-			distSquared = xDist*xDist + yDist*yDist
-			#Check the squared distances instead of the the distances, same result, but avoids a square root.
-			if(distSquared <= (self.radius + obj.radius)*(self.radius + obj.radius)):
-				velxocity = obj.velx - self.velx
-				velyocity = obj.vely - self.vely
-				dotProduct = xDist*velxocity + yDist*velyocity
-				#Neat vector maths, used for checking if the objects moves towards one another.
-				if(dotProduct > 0):
-					collisionScale = dotProduct / distSquared
-					xCollision = xDist * collisionScale
-					yCollision = yDist * collisionScale
-					#The Collision vector is the speed difference projected on the Dist vector,
-					#thus it is the component of the speed difference needed for the collision.
-					combinedMass = self.mass + obj.mass
-					collisionWeightself = 2 * obj.mass / combinedMass
-					collisionWeightobj = 2 * self.mass / combinedMass
-					self.velx += collisionWeightself * xCollision
-					self.vely += collisionWeightself * yCollision
-					obj.velx -= collisionWeightobj * xCollision
-					obj.vely -= collisionWeightobj * yCollision
+			# xDist = self.x - obj.x
+			# yDist = self.y - obj.y
+			# distSquared = xDist*xDist + yDist*yDist
+			# #Check the squared distances instead of the the distances, same result, but avoids a square root.
+			# if(distSquared <= (self.radius + obj.radius)*(self.radius + obj.radius)):
+			# 	velxocity = obj.velx - self.velx
+			# 	velyocity = obj.vely - self.vely
+			# 	dotProduct = xDist*velxocity + yDist*velyocity
+			# 	#Neat vector maths, used for checking if the objects moves towards one another.
+			# 	if(dotProduct > 0):
+			# 		collisionScale = dotProduct / distSquared
+			# 		xCollision = xDist * collisionScale
+			# 		yCollision = yDist * collisionScale
+			# 		#The Collision vector is the speed difference projected on the Dist vector,
+			# 		#thus it is the component of the speed difference needed for the collision.
+			# 		combinedMass = self.mass + obj.mass
+			# 		collisionWeightself = 2 * obj.mass / combinedMass
+			# 		collisionWeightobj = 2 * self.mass / combinedMass
+			# 		self.velx += collisionWeightself * xCollision
+			# 		self.vely += collisionWeightself * yCollision
+			# 		obj.velx -= collisionWeightobj * xCollision
+			# 		obj.vely -= collisionWeightobj * yCollision
    #              
    #          
 
